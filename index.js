9dc8c4ecc33aa40cfa611bcb1f06f1183dd1f323
devanandd7 <devanandutkarsh7@gmail.com>
2024-09-05 23:24:03 +0530

    server setup through express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+app.get('/', (req, res) => {               
+  res.send('Hello World! ram')
+})
+
+app.listen(port,()=>{
+    console.log(`online on ${port}`)
+})
